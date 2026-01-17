8ba25db55c799b15f4a86d2f6146478fac436f95
Ibraheem AbdulRahman <ibraheemabdulrahman305@gmail.com>
2026-01-17 15:34:32 -0800

    solve the accordionContent error nothing much

--- components/decision-matrix/accordion-list.tsx
+++ components/decision-matrix/accordion-list.tsx
@@ -1,25 +1,41 @@
 import {
-    Accordion,
-    AccordionContent,
-    AccordionItem,
-    AccordionTrigger,
-} from "@/components/ui/accordion"
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
-
-export default function AccordionList({ name, list } : { name: string, list: string[]}) {
+export default function AccordionList({
+  name,
+  list,
+}: {
+  name: string;
+  list: string[];
+}) {
   return (
-    <Accordion type="single" collapsible className="w-full border-b border-border mb-2">
+    <Accordion
+      type="single"
+      collapsible
+      className="w-full border-b border-border mb-2"
+    >
       <AccordionItem value={name}>
         <AccordionTrigger className="py-2 sm:py-2.5 bg-transparent text-xs sm:text-sm font-medium hover:text-foreground transition-colors">
           {name.charAt(0).toUpperCase() + name.slice(1)}
         </AccordionTrigger>
-        <AccordionContent>
-          <ul className="text-xs sm:text-sm gap-1.5 flex flex-col pl-2 border-l-2 border-accent-border">
-            {list.map((item, index) => (
-              <li key={index} className="wrap-break-words pl-2 leading-relaxed">{item}</li>
-            ))}
-          </ul>
-        </AccordionContent>
+        {list && list.length > 0 && (
+          <AccordionContent>
+            <ul className="text-xs sm:text-sm gap-1.5 flex flex-col pl-2 border-l-2 border-accent-border">
+              {list.map((item, index) => (
+                <li
+                  key={index}
+                  className="wrap-break-words pl-2 leading-relaxed"
+                >
+                  {item}
+                </li>
+              ))}
+            </ul>
+          </AccordionContent>
+        )}
       </AccordionItem>
     </Accordion>
   );
